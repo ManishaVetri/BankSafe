b555b6d92131236c873d27d6e838f08388382cd4
ManishaVetri <dummy@gmail.com>
2026-01-16 13:11:31 +0000

    solve initial commit

---
+++ server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
@@ -0,0 +1,10 @@
+package com.edutech.progressive.config;
+
+public class DatabaseConnectionManager {
+ private static void loadProperties(){
+ spring.datasource.url="jdbc:mysql://localhost:3306/mydb";
+spring.datasource.username="root";
+spring.datasource.password="root";
+spring.datasource.driver-class-name="com.mysql.cj.jdbc.Driver";
+ }
+}
