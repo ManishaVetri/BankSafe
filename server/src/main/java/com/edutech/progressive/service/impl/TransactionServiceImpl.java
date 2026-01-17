39e9aadd5966016be69d146614b3ad3a05bf71b1
ManishaVetri <dummy@gmail.com>
2026-01-17 03:37:18 +0000

    Day 1 Task

--- server/src/main/java/com/edutech/progressive/service/impl/TransactionServiceImpl.java
+++ server/src/main/java/com/edutech/progressive/service/impl/TransactionServiceImpl.java
@@ -1,5 +1,42 @@
 package com.edutech.progressive.service.impl;
 
-public class TransactionServiceImpl {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.edutech.progressive.entity.Transactions;
+import com.edutech.progressive.service.TransactionService;
+
+public class TransactionServiceImpl implements TransactionService {
+
+    @Override
+    public List<Transactions> getAllTransactions() throws SQLException {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public Transactions getTransactionById(int transactionId) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public int addTransaction(Transactions transaction) throws SQLException {
+        return -1;
+    }
+
+    @Override
+    public void updateTransaction(Transactions transaction) throws SQLException {
+       
+    }
+
+    @Override
+    public void deleteTransaction(int transactionId) throws SQLException {
+       
+    }
+
+    @Override
+    public List<Transactions> getTransactionsByCustomerId(int customerId) throws SQLException {
+      return  null;
+    }
     
 }
