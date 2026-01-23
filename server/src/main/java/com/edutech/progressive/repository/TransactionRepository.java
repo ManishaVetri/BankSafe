bf4076f240ae5dc14841b3ad721aff22bdb43574
ManishaVetri <dummy@gmail.com>
2026-01-23 09:42:22 +0000

    Day 8 Task

--- server/src/main/java/com/edutech/progressive/repository/TransactionRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/TransactionRepository.java
@@ -1,8 +1,11 @@
 package com.edutech.progressive.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edutech.progressive.entity.Transactions;
 
 public interface TransactionRepository extends JpaRepository<Transactions,Integer> {
+     List<Transactions> findByAccountsAccountId(int accountId);
 }
